2ca86f2eb918b56a68f8a64fe159cb87328d6e1f
Matthew <matthewgao@gmail.com>
2013-04-23 17:28:23 +0800

    cc

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include "ThreadPool.h"
+#include <unistd.h>  //for sleeping
+
+
+void *run(void *arg){
+  int *num=(int*)arg;
+  //sleep(*num);
+  printf("Thread is %x,  Num %d \n",pthread_self(),*num);
+  //  sleep(5);
+}
+
+int main(){
+  ThreadPool *tp=new ThreadPool();
+  int *num=(int*)malloc(sizeof(int)*19);
+  for(int i=0;i<19;i++){
+    num[i]=i;
+    tp->addJob(run,&num[i]);
+    //tp->start();
+  }
+  printf("MAIN::adding job\n");
+  tp->startAll();
+  //tp->start();
+  //tp->start();
+  //tp->start();
+  //tp->start();
+
+  sleep(5);
+  //tp->destoryPool();
+  delete tp;
+  
+  return 0;
+}
