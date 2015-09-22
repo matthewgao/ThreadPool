3c742515d4ca4fae0cecd4ad2cfbe805120470bf
Matthew <matthewgao@gmail.com>
2015-09-22 23:25:09 +0800

    Mutex lock

---
+++ MutexLock.cpp
@@ -0,0 +1,10 @@
+/*
+ * MutexLock.cpp
+ * Created By: Matthew Gao
+ * Copyright (c) Dell.Inc
+ * Created Time: Tue Sep 22 23:24:43 2015
+ */
+
+#include<iostream>
+using namespace std;
+
---
+++ MutexLock.h
@@ -0,0 +1,18 @@
+/*
+ * MutexLock.h
+ * Created By: Matthew Gao
+ * Copyright (c) Dell.Inc
+ * Created Time: Tue Sep 22 23:23:54 2015
+ */
+
+#ifndef _MUTEXLOCK_H
+#define _MUTEXLOCK_H
+
+#include <pthread.h>
+
+class MutexLock{
+
+}
+
+
+#endif
